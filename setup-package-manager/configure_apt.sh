1ba768b11b6edc77d0aa660f33ad66264ae01c06
Enrico Seiler <enrico.seiler@hotmail.de>
2023-06-10 10:37:05 +0200

    [MISC] Also setup llvm apt

--- setup-package-manager/configure_apt.sh
+++ setup-package-manager/configure_apt.sh
@@ -5,4 +5,6 @@ set -Eeuo pipefail
 sudo sed -i 's@azure.archive.ubuntu.com@mirror.enzu.com@' /etc/apt/sources.list
 sudo add-apt-repository --no-update --yes ppa:ubuntu-toolchain-r/ppa
 sudo add-apt-repository --no-update --yes ppa:ubuntu-toolchain-r/test
+wget -O - https://apt.llvm.org/llvm-snapshot.gpg.key | sudo apt-key add -
+sudo add-apt-repository --no-update --yes "deb http://apt.llvm.org/jammy/ llvm-toolchain-jammy main"
 sudo apt-get update
