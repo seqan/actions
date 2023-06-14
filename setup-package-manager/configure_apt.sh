ab21a4d34ecc1a2e79274b23525f83f6d5fca467
Enrico Seiler <enrico.seiler@hotmail.de>
2023-06-14 14:52:35 +0200

    Revert "[MISC] remove llvm15, add jammy-updates"
    This reverts commit 86cd38f09ff5f7d5d6aecdc203c3159055699205.

--- setup-package-manager/configure_apt.sh
+++ setup-package-manager/configure_apt.sh
@@ -3,10 +3,10 @@ set -Eeuo pipefail
 
 # Replace Microsofts repository with some other mirror, because azure is sometimes down.
 sudo sed -i 's@azure.archive.ubuntu.com@mirror.enzu.com@' /etc/apt/sources.list
-sudo add-apt-repository --no-update --yes "deb http://mirror.enzu.com/ubuntu/ jammy-updates main restricted universe multiverse"
 sudo add-apt-repository --no-update --yes ppa:ubuntu-toolchain-r/ppa
 sudo add-apt-repository --no-update --yes ppa:ubuntu-toolchain-r/test
 wget -qO- https://apt.llvm.org/llvm-snapshot.gpg.key | sudo tee /etc/apt/trusted.gpg.d/apt.llvm.org.asc
+sudo add-apt-repository --no-update --yes "deb http://apt.llvm.org/jammy/ llvm-toolchain-jammy-15 main"
 sudo add-apt-repository --no-update --yes "deb http://apt.llvm.org/jammy/ llvm-toolchain-jammy-16 main"
 sudo add-apt-repository --no-update --yes "deb http://apt.llvm.org/jammy/ llvm-toolchain-jammy main"
 sudo apt-get update
