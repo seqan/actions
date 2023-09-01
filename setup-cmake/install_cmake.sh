2b8e1866d23acfef70aef1cc900981e4b56f3863
Enrico Seiler <enrico.seiler@hotmail.de>
2023-09-01 17:37:24 +0200

    [MISC] Handle new CMake packages

--- setup-cmake/install_cmake.sh
+++ setup-cmake/install_cmake.sh
@@ -3,18 +3,22 @@ set -Eeuo pipefail
 
 CMAKE_VERSION="$1"
 
-if [ "$RUNNER_OS" == "Linux" ]; then
-    OS="Linux"
-    CMAKE_PATH="/tmp/cmake-${CMAKE_VERSION}-${OS}-x86_64/bin"
-elif [ "$RUNNER_OS" == "macOS" ]; then
-    OS="Darwin"
-    CMAKE_PATH="/tmp/cmake-${CMAKE_VERSION}-${OS}-x86_64/CMake.app/Contents/bin"
+if [ "${RUNNER_OS}" == "Linux" ]; then
+    OS="Linux-x86_64"
+    CMAKE_PATH="/tmp/cmake-${CMAKE_VERSION}-${OS}/bin"
+elif [ "${RUNNER_OS}" == "macOS" ]; then
+    if [[ "${CMAKE_VERSION}" > "3.19.1" ]]; then
+        OS="macos-universal"
+    else
+        OS="Darwin-x86_64"
+    fi
+    CMAKE_PATH="/tmp/cmake-${CMAKE_VERSION}-${OS}/CMake.app/Contents/bin"
 else
     echo "OS ${$RUNNER_OS} is not supported"
     exit 1
 fi
 
 mkdir -p /tmp/cmake-download
-wget --retry-connrefused --waitretry=30 --read-timeout=30 --timeout=30 --tries=20 --no-clobber --quiet --directory-prefix=/tmp/cmake-download/ https://github.com/Kitware/CMake/releases/download/v${CMAKE_VERSION}/cmake-${CMAKE_VERSION}-${OS}-x86_64.tar.gz
-tar -C /tmp/ -zxf /tmp/cmake-download/cmake-${CMAKE_VERSION}-${OS}-x86_64.tar.gz
+wget --retry-connrefused --waitretry=30 --read-timeout=30 --timeout=30 --tries=20 --no-clobber --quiet --directory-prefix=/tmp/cmake-download/ https://github.com/Kitware/CMake/releases/download/v${CMAKE_VERSION}/cmake-${CMAKE_VERSION}-${OS}.tar.gz
+tar -C /tmp/ -zxf /tmp/cmake-download/cmake-${CMAKE_VERSION}-${OS}.tar.gz
 echo "${CMAKE_PATH}" >> $GITHUB_PATH # Only available in subsequent steps!
