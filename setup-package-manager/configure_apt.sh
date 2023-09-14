bfc9f081825a55f40aeb97a46323b8be2429e7c8
Enrico Seiler <enrico.seiler@hotmail.de>
2023-09-14 17:52:06 +0200

    [MISC] Apt mirror file changed

--- setup-package-manager/configure_apt.sh
+++ setup-package-manager/configure_apt.sh
@@ -2,7 +2,7 @@
 set -Eeuo pipefail
 
 # Replace Microsofts repository with some other mirror, because azure is sometimes down.
-sudo sed -i 's@azure.archive.ubuntu.com@mirror.enzu.com@' /etc/apt/sources.list
+sudo sed -i 's@azure.archive.ubuntu.com@mirror.enzu.com@' /etc/apt/apt-mirrors.txt
 sudo add-apt-repository --no-update --yes ppa:ubuntu-toolchain-r/ppa
 sudo add-apt-repository --no-update --yes ppa:ubuntu-toolchain-r/test
 wget -qO- https://apt.llvm.org/llvm-snapshot.gpg.key | sudo tee /etc/apt/trusted.gpg.d/apt.llvm.org.asc
