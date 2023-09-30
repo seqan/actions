50283e89302fc837f3763183373a44e671220e9a
Enrico Seiler <enrico.seiler@hotmail.de>
2023-09-30 16:27:17 +0200

    [FIX] Intel GPG key expired

--- setup-package-manager/configure_apt.sh
+++ setup-package-manager/configure_apt.sh
@@ -12,4 +12,4 @@ sudo add-apt-repository --no-update --yes "deb http://apt.llvm.org/jammy/ llvm-t
 sudo add-apt-repository --no-update --yes "deb http://apt.llvm.org/jammy/ llvm-toolchain-jammy main"
 wget -O- https://apt.repos.intel.com/intel-gpg-keys/GPG-PUB-KEY-INTEL-SW-PRODUCTS.PUB | sudo tee /etc/apt/trusted.gpg.d/apt.repos.intel.com.asc
 sudo add-apt-repository --no-update --yes "deb https://apt.repos.intel.com/oneapi all main"
-sudo apt-get update
+sudo apt-get -o Acquire::AllowInsecureRepositories=true update
