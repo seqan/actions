5eebee6af666b43708f0789a79ea2040401910c2
Enrico Seiler <enrico.seiler@hotmail.de>
2023-11-30 14:25:51 +0100

    Retry apt commands

--- setup-package-manager/configure_apt.sh
+++ setup-package-manager/configure_apt.sh
@@ -4,22 +4,32 @@ set -Eeuo pipefail
 # Contains, e.g., VERSION_CODENAME
 source /etc/os-release
 
+echo 'Acquire::Retries "10";' | sudo tee /etc/apt/apt.conf.d/80-retries
+
+retry_command ()
+{
+    for i in 1 2 3 4; do
+        "$@" && break || sleep $((i * 5))
+    done
+    "$@"
+}
+
 # Replace Microsofts repository with some other mirror, because azure is sometimes down.
-sudo sed -i 's@azure.archive.ubuntu.com@mirror.enzu.com@' /etc/apt/apt-mirrors.txt
+retry_command sudo sed -i 's@azure.archive.ubuntu.com@mirror.enzu.com@' /etc/apt/apt-mirrors.txt
 
 # GCC
-sudo add-apt-repository --no-update --yes ppa:ubuntu-toolchain-r/ppa
-sudo add-apt-repository --no-update --yes ppa:ubuntu-toolchain-r/test
+retry_command sudo add-apt-repository --no-update --yes --ppa ppa:ubuntu-toolchain-r/ppa
+retry_command sudo add-apt-repository --no-update --yes --ppa ppa:ubuntu-toolchain-r/test
 
 # Clang
-wget -qO- https://apt.llvm.org/llvm-snapshot.gpg.key | sudo tee /etc/apt/trusted.gpg.d/apt.llvm.org.asc
-sudo add-apt-repository --no-update --yes "deb http://apt.llvm.org/${VERSION_CODENAME}/ llvm-toolchain-${VERSION_CODENAME}-15 main"
-sudo add-apt-repository --no-update --yes "deb http://apt.llvm.org/${VERSION_CODENAME}/ llvm-toolchain-${VERSION_CODENAME}-16 main"
-sudo add-apt-repository --no-update --yes "deb http://apt.llvm.org/${VERSION_CODENAME}/ llvm-toolchain-${VERSION_CODENAME}-17 main"
-sudo add-apt-repository --no-update --yes "deb http://apt.llvm.org/${VERSION_CODENAME}/ llvm-toolchain-${VERSION_CODENAME} main"
+retry_command wget -qO- https://apt.llvm.org/llvm-snapshot.gpg.key | sudo tee /dev/shm/test.asc
+retry_command sudo add-apt-repository --no-update --yes --sourceslist "deb http://apt.llvm.org/${VERSION_CODENAME}/ llvm-toolchain-${VERSION_CODENAME}-15 main"
+retry_command sudo add-apt-repository --no-update --yes --sourceslist "deb http://apt.llvm.org/${VERSION_CODENAME}/ llvm-toolchain-${VERSION_CODENAME}-16 main"
+retry_command sudo add-apt-repository --no-update --yes --sourceslist "deb http://apt.llvm.org/${VERSION_CODENAME}/ llvm-toolchain-${VERSION_CODENAME}-17 main"
+retry_command sudo add-apt-repository --no-update --yes --sourceslist "deb http://apt.llvm.org/${VERSION_CODENAME}/ llvm-toolchain-${VERSION_CODENAME} main"
 
 # Intel
-wget -O- https://apt.repos.intel.com/intel-gpg-keys/GPG-PUB-KEY-INTEL-SW-PRODUCTS.PUB | sudo tee /etc/apt/trusted.gpg.d/apt.repos.intel.com.asc
-sudo add-apt-repository --no-update --yes "deb https://apt.repos.intel.com/oneapi all main"
+retry_command wget -O- https://apt.repos.intel.com/intel-gpg-keys/GPG-PUB-KEY-INTEL-SW-PRODUCTS.PUB | sudo tee /etc/apt/trusted.gpg.d/apt.repos.intel.com.asc
+retry_command sudo add-apt-repository --no-update --yes --sourceslist "deb https://apt.repos.intel.com/oneapi all main"
 
-sudo apt-get update
+retry_command sudo apt-get update
