811ab85e786bfe6d519ecbb47875cba5168f172a
Enrico Seiler <enrico.seiler@hotmail.de>
2023-11-30 14:37:40 +0100

    Use old add-apt-repository format for 20.04 compatibility

--- setup-package-manager/configure_apt.sh
+++ setup-package-manager/configure_apt.sh
@@ -18,18 +18,18 @@ retry_command ()
 retry_command sudo sed -i 's@azure.archive.ubuntu.com@mirror.enzu.com@' /etc/apt/apt-mirrors.txt
 
 # GCC
-retry_command sudo add-apt-repository --no-update --yes --ppa ppa:ubuntu-toolchain-r/ppa
-retry_command sudo add-apt-repository --no-update --yes --ppa ppa:ubuntu-toolchain-r/test
+retry_command sudo add-apt-repository --no-update --yes ppa:ubuntu-toolchain-r/ppa
+retry_command sudo add-apt-repository --no-update --yes ppa:ubuntu-toolchain-r/test
 
 # Clang
 retry_command wget -qO- https://apt.llvm.org/llvm-snapshot.gpg.key | sudo tee /etc/apt/trusted.gpg.d/apt.llvm.org.asc
-retry_command sudo add-apt-repository --no-update --yes --sourceslist "deb http://apt.llvm.org/${VERSION_CODENAME}/ llvm-toolchain-${VERSION_CODENAME}-15 main"
-retry_command sudo add-apt-repository --no-update --yes --sourceslist "deb http://apt.llvm.org/${VERSION_CODENAME}/ llvm-toolchain-${VERSION_CODENAME}-16 main"
-retry_command sudo add-apt-repository --no-update --yes --sourceslist "deb http://apt.llvm.org/${VERSION_CODENAME}/ llvm-toolchain-${VERSION_CODENAME}-17 main"
-retry_command sudo add-apt-repository --no-update --yes --sourceslist "deb http://apt.llvm.org/${VERSION_CODENAME}/ llvm-toolchain-${VERSION_CODENAME} main"
+retry_command sudo add-apt-repository --no-update --yes "deb http://apt.llvm.org/${VERSION_CODENAME}/ llvm-toolchain-${VERSION_CODENAME}-15 main"
+retry_command sudo add-apt-repository --no-update --yes "deb http://apt.llvm.org/${VERSION_CODENAME}/ llvm-toolchain-${VERSION_CODENAME}-16 main"
+retry_command sudo add-apt-repository --no-update --yes "deb http://apt.llvm.org/${VERSION_CODENAME}/ llvm-toolchain-${VERSION_CODENAME}-17 main"
+retry_command sudo add-apt-repository --no-update --yes "deb http://apt.llvm.org/${VERSION_CODENAME}/ llvm-toolchain-${VERSION_CODENAME} main"
 
 # Intel
 retry_command wget -O- https://apt.repos.intel.com/intel-gpg-keys/GPG-PUB-KEY-INTEL-SW-PRODUCTS.PUB | sudo tee /etc/apt/trusted.gpg.d/apt.repos.intel.com.asc
-retry_command sudo add-apt-repository --no-update --yes --sourceslist "deb https://apt.repos.intel.com/oneapi all main"
+retry_command sudo add-apt-repository --no-update --yes "deb https://apt.repos.intel.com/oneapi all main"
 
 retry_command sudo apt-get update
