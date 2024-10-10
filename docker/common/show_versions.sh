2d8c48bfdae7ec026ef1988094fc6f7060c2d01f
Enrico Seiler <enrico.seiler@hotmail.de>
2024-10-10 16:38:27 +0200

    Document Package Versions

---
+++ docker/common/show_versions.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -l
+
+# SPDX-FileCopyrightText: 2006-2024 Knut Reinert & Freie Universität Berlin
+# SPDX-FileCopyrightText: 2016-2024 Knut Reinert & MPI für molekulare Genetik
+# SPDX-License-Identifier: CC-BY-4.0
+
+apt list --manual-installed 2>/dev/null | \
+    grep -v "Listing..." | \
+    sed -E 's@(\S+)/\S+ (\S+) \S+ \S+@\1\t\2@g' > /manually_installed_packages.version
+
+apt list --installed 2>/dev/null | \
+    grep -v "Listing..." | \
+    sed -E 's@(\S+)/\S+ (\S+) \S+ \S+@\1\t\2@g' > /installed_packages.version
