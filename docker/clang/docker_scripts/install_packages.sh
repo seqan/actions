72caa480ca62cc213d5f95d763f0132503fe590e
Enrico Seiler <enrico.seiler@hotmail.de>
2024-12-09 17:20:44 +0100

    [MISC] Overwrite python dependency for libomp-16-dev

--- docker/clang/docker_scripts/install_packages.sh
+++ docker/clang/docker_scripts/install_packages.sh
@@ -20,9 +20,23 @@ apt-get install --yes --no-install-recommends \
     libc++abi-${CLANG_VERSION}-dev \
     libclang-${CLANG_VERSION}-dev \
     libclang-rt-${CLANG_VERSION}-dev \
-    libomp-${CLANG_VERSION}-dev \
     lld-${CLANG_VERSION} \
     llvm-${CLANG_VERSION} \
     llvm-${CLANG_VERSION}-dev
+
+# Overwrite libpython3.11 dependency for libomp-16-dev.
+if [[ CLANG_VERSION -eq 16 ]]; then
+    cd /tmp
+    apt-get download libomp-16-dev
+    dpkg-deb -x libomp-16-dev*.deb libomp-16-dev
+    dpkg-deb --control libomp-16-dev*.deb libomp-16-dev/DEBIAN
+    rm -f libomp-16-dev*.deb
+    sed -i 's@libpython3.11 (>= 3.11.5)@libpython3.12 (>= 3.12.0)@g' libomp-16-dev/DEBIAN/control
+    dpkg -b libomp-16-dev libomp-16-dev.deb
+    apt-get install --yes --no-install-recommends ./libomp-16-dev.deb
+    rm -f libomp-16-dev.deb
+else
+    apt-get install --yes --no-install-recommends libomp-${CLANG_VERSION}-dev
+fi
 
 rm -rf /var/lib/apt/lists/*
