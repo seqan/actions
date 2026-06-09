2d3f42f4e630ab4b1eec22410d2fa4d6163bc36e
Enrico Seiler <eseiler@users.noreply.github.com>
2026-06-09 14:45:36 +0200

    Merge pull request #155 from eseiler/fix/sde
    fix: sde download

--- docker/common/install_sde.sh
+++ docker/common/install_sde.sh
@@ -7,7 +7,8 @@
 set -Eeuxo pipefail
 
 SDE_EXTRACTION_PATH=$1
-SDE_TARGET_FILE="/tmp/sde.tar.xz"
+SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
+SDE_ARCHIVE="${SCRIPT_DIR}/sde.tar.xz"
 
 # Fail the script if the SDE_EXTRACTION_PATH is empty
 if [[ -z "${SDE_EXTRACTION_PATH}" ]]; then
@@ -15,25 +16,14 @@ if [[ -z "${SDE_EXTRACTION_PATH}" ]]; then
     exit 1
 fi
 
-# Download the SDE tarball
-# Update the following variables if the SDE version changes
-DIR_PATH="913594"
-SDE_VERSION="10.7.0"
-SDE_DATE="2026-02-18"
-
-# Example: https://downloadmirror.intel.com/913594/sde-external-10.7.0-2026-02-18-lin.tar.xz
-FULL_URL="https://downloadmirror.intel.com/${DIR_PATH}/sde-external-${SDE_VERSION}-${SDE_DATE}-lin.tar.xz"
-
-wget --quiet --retry-connrefused --output-document "${SDE_TARGET_FILE}" "${FULL_URL}"
-
 # Create the extraction directory if it doesn't exist
 mkdir -p "${SDE_EXTRACTION_PATH}"
 
 # Extract the tarball file into the extraction directory
-tar xf "${SDE_TARGET_FILE}" -C "${SDE_EXTRACTION_PATH}" --strip-components=1
+tar xf "${SDE_ARCHIVE}" -C "${SDE_EXTRACTION_PATH}" --strip-components=1
 
 # Clean up
-rm "${SDE_TARGET_FILE}"
+rm "${SDE_ARCHIVE}"
 
 # Add the SDE version to the versions file
-echo -e "intel-sde*\t${SDE_VERSION}" | tee -a /manually_installed_packages.version /installed_packages.version > /dev/null
+echo -e "intel-sde*\t$(sde --version | grep -oP 'Version:\s*\K[0-9.]+')" | tee -a /manually_installed_packages.version /installed_packages.version > /dev/null
